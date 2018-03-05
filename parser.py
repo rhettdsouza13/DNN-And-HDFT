1ddc79aa5f2ac2051f89adaf1ca35c6c42e12f2e
Rhett D'souza <rhettdsouza13@gmail.com>
2018-03-05 17:34:10 -0500

    PATH Set Incorporated

--- parser.py
+++ parser.py
@@ -2,147 +2,240 @@ import struct
 import numpy
 from PIL import Image, ImageOps
 
+dat_set = 'PATH'
+
+inputs = []
+labels = []
+inputsTst = []
+labelsTst = []
+inputs_fin_c = []
+labels_c = []
+inputsTst_fin_c = []
+labelsTst_c = []
+inputs_fin_p = []
+labels_p = []
+inputsTst_fin_p = []
+labelsTst_p = []
+
+if dat_set == 'MNIST' :
+    with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/MINST.train.bin", "rb") as bfile:
+        num = bfile.read(4)
+        xdim = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ydim = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ccod = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        num = bfile.read(4)
+        num = bfile.read(4)
+        henc = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        nsamp = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        inpsize = struct.unpack('i', num)[0]
+
+        labels=[]
+        for i in xrange(nsamp):
+            num = bfile.read(1)
+            val = struct.unpack('B', num)[0]
+            label = [0 for i in xrange(10)]
+            label[val] = 1
+            labels.append(label)
+
+        inputs=[]
+        for i in xrange(nsamp):
+            nums = bfile.read(inpsize*4)
+            inp = list(struct.unpack('1024f', nums))
+            inputs.append(inp)
+
+    with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/MINST.test.bin", "rb") as bfile:
+        num = bfile.read(4)
+        xdim = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ydim = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ccod = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        num = bfile.read(4)
+        num = bfile.read(4)
+        henc = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        nsamp = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        inpsize = struct.unpack('i', num)[0]
+
+        labelsTst=[]
+        for i in xrange(nsamp):
+            num = bfile.read(1)
+            val = struct.unpack('B', num)[0]
+            label = [0 for i in xrange(10)]
+            label[val] = 1
+            labelsTst.append(label)
+
+        inputsTst=[]
+        for i in xrange(nsamp):
+
+            nums = bfile.read(inpsize*4)
+
+            inp = list(struct.unpack('1024f', nums))
+            inputsTst.append(inp)
+
+if dat_set == 'CIFAR' :
+    with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin", "rb") as bfile:
+        #"/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin"
+        num = bfile.read(4)
+        xdim_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ydim_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ccod_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        num = bfile.read(4)
+        num = bfile.read(4)
+        henc_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        nsamp_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        inpsize_c = struct.unpack('i', num)[0]
+
+        print xdim_c, ydim_c, ccod_c, henc_c, nsamp_c, inpsize_c
+
+        labels_c=[]
+        for i in xrange(nsamp_c):
+            num = bfile.read(1)
+            val = struct.unpack('B', num)[0]
+            label = [0 for i in xrange(10)]
+            label[val] = 1
+            labels_c.append(label)
+        print labels_c[0]
+
+        inputs_fin_c=[]
+        for i in xrange(nsamp_c):
+            inputs_c = []
+            nums = bfile.read(inpsize_c*4)
+            #print ord(nums)
+            inp = list(struct.unpack('3072f', nums))
+            inputs_c = [pix_val for pix_val in inp]
+
+            inputs_fin_c.append(inputs_c)
+        print len(inputs_fin_c)
+
+    with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.test.bin", "rb") as bfile:
+        #"/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin"
+        num = bfile.read(4)
+        xdim_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ydim_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ccod_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        num = bfile.read(4)
+        num = bfile.read(4)
+        henc_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        nsamp_c = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        inpsize_c = struct.unpack('i', num)[0]
+
+        print xdim_c, ydim_c, ccod_c, henc_c, nsamp_c, inpsize_c
+
+        labelsTst_c=[]
+        for i in xrange(nsamp_c):
+            num = bfile.read(1)
+            val = struct.unpack('B', num)[0]
+            label = [0 for i in xrange(10)]
+            label[val] = 1
+            labelsTst_c.append(label)
+        print labelsTst_c[0]
+
+        inputsTst_fin_c=[]
+        for i in xrange(nsamp_c):
+            inputs_c = []
+            nums = bfile.read(inpsize_c*4)
+            #print ord(nums)
+            inp = list(struct.unpack('3072f', nums))
+            inputs_c = [pix_val for pix_val in inp]
+
+            inputsTst_fin_c.append(inputs_c)
+
+if dat_set == 'PATH':
+    with open("/home/hdft/Documents/Data_ML/PATH_DATA.bin", "rb") as bfile:
+        #"/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin"
+        num = bfile.read(4)
+        xdim_p = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ydim_p = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        ccod_p = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        num = bfile.read(4)
+        num = bfile.read(4)
+        henc_p = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        nsamp_p = struct.unpack('i', num)[0]
+        num = bfile.read(4)
+        inpsize_p = struct.unpack('i', num)[0]
+
+        print xdim_p, ydim_p, ccod_p, henc_p, nsamp_p, inpsize_p
+
+        labels_p=[]
+        for i in xrange(nsamp_p):
+            num = bfile.read(1)
+            val = struct.unpack('B', num)[0]
+            label = [0 for i in xrange(2)]
+            label[val] = 1
+            labels_p.append(label)
+        print labels_p
+
+        inputs_fin_p=[]
+        for i in xrange(nsamp_p):
+            inputs_p = []
+            nums = bfile.read(inpsize_p*4)
+            #print ord(nums)
+            inp = list(struct.unpack('12288f', nums))
+            inputs_p = [pix_val for pix_val in inp]
+
+            inputs_fin_p.append(inputs_p)
+        print len(inputs_fin_p)
 
 
-with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/MINST.train.bin", "rb") as bfile:
-    num = bfile.read(4)
-    xdim = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ydim = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ccod = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    num = bfile.read(4)
-    num = bfile.read(4)
-    henc = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    nsamp = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    inpsize = struct.unpack('i', num)[0]
-
-    labels=[]
-    for i in xrange(nsamp):
-        num = bfile.read(1)
-        val = struct.unpack('B', num)[0]
-        label = [0 for i in xrange(10)]
-        label[val] = 1
-        labels.append(label)
-
-    inputs=[]
-    for i in xrange(nsamp):
-        nums = bfile.read(inpsize*4)
-        inp = list(struct.unpack('1024f', nums))
-        inputs.append(inp)
-
-with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/MINST.test.bin", "rb") as bfile:
-    num = bfile.read(4)
-    xdim = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ydim = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ccod = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    num = bfile.read(4)
-    num = bfile.read(4)
-    henc = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    nsamp = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    inpsize = struct.unpack('i', num)[0]
-
-    labelsTst=[]
-    for i in xrange(nsamp):
-        num = bfile.read(1)
-        val = struct.unpack('B', num)[0]
-        label = [0 for i in xrange(10)]
-        label[val] = 1
-        labelsTst.append(label)
-
-    inputsTst=[]
-    for i in xrange(nsamp):
-
-        nums = bfile.read(inpsize*4)
-
-        inp = list(struct.unpack('1024f', nums))
-        inputsTst.append(inp)
-
-
-with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin", "rb") as bfile:
-    #"/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin"
-    num = bfile.read(4)
-    xdim_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ydim_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ccod_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    num = bfile.read(4)
-    num = bfile.read(4)
-    henc_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    nsamp_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    inpsize_c = struct.unpack('i', num)[0]
-
-    print xdim_c, ydim_c, ccod_c, henc_c, nsamp_c, inpsize_c
-
-    labels_c=[]
-    for i in xrange(nsamp_c):
-        num = bfile.read(1)
-        val = struct.unpack('B', num)[0]
-        label = [0 for i in xrange(10)]
-        label[val] = 1
-        labels_c.append(label)
-    print labels_c[0]
-
-    inputs_fin_c=[]
-    for i in xrange(nsamp_c):
-        inputs_c = []
-        nums = bfile.read(inpsize_c*4)
-        #print ord(nums)
-        inp = list(struct.unpack('3072f', nums))
-        inputs_c = [pix_val for pix_val in inp]
-
-        inputs_fin_c.append(inputs_c)
-    print len(inputs_fin_c)
-
-with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.test.bin", "rb") as bfile:
-    #"/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin"
-    num = bfile.read(4)
-    xdim_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ydim_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    ccod_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    num = bfile.read(4)
-    num = bfile.read(4)
-    henc_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    nsamp_c = struct.unpack('i', num)[0]
-    num = bfile.read(4)
-    inpsize_c = struct.unpack('i', num)[0]
-
-    print xdim_c, ydim_c, ccod_c, henc_c, nsamp_c, inpsize_c
-
-    labelsTst_c=[]
-    for i in xrange(nsamp_c):
-        num = bfile.read(1)
-        val = struct.unpack('B', num)[0]
-        label = [0 for i in xrange(10)]
-        label[val] = 1
-        labelsTst_c.append(label)
-    print labelsTst_c[0]
-
-    inputsTst_fin_c=[]
-    for i in xrange(nsamp_c):
-        inputs_c = []
-        nums = bfile.read(inpsize_c*4)
-        #print ord(nums)
-        inp = list(struct.unpack('3072f', nums))
-        inputs_c = [pix_val for pix_val in inp]
-
-        inputsTst_fin_c.append(inputs_c)
+    # with open("/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.test.bin", "rb") as bfile:
+    #     #"/home/hdft/Documents/Data_ML/2017_07_03 CNN_BIN/CIFAR.train.bin"
+    #     num = bfile.read(4)
+    #     xdim_c = struct.unpack('i', num)[0]
+    #     num = bfile.read(4)
+    #     ydim_c = struct.unpack('i', num)[0]
+    #     num = bfile.read(4)
+    #     ccod_c = struct.unpack('i', num)[0]
+    #     num = bfile.read(4)
+    #     num = bfile.read(4)
+    #     num = bfile.read(4)
+    #     henc_c = struct.unpack('i', num)[0]
+    #     num = bfile.read(4)
+    #     nsamp_c = struct.unpack('i', num)[0]
+    #     num = bfile.read(4)
+    #     inpsize_c = struct.unpack('i', num)[0]
+    #
+    #     print xdim_c, ydim_c, ccod_c, henc_c, nsamp_c, inpsize_c
+    #
+    #     labelsTst_c=[]
+    #     for i in xrange(nsamp_c):
+    #         num = bfile.read(1)
+    #         val = struct.unpack('B', num)[0]
+    #         label = [0 for i in xrange(10)]
+    #         label[val] = 1
+    #         labelsTst_c.append(label)
+    #     print labelsTst_c[0]
+    #
+    #     inputsTst_fin_c=[]
+    #     for i in xrange(nsamp_c):
+    #         inputs_c = []
+    #         nums = bfile.read(inpsize_c*4)
+    #         #print ord(nums)
+    #         inp = list(struct.unpack('3072f', nums))
+    #         inputs_c = [pix_val for pix_val in inp]
+    #
+    #         inputsTst_fin_c.append(inputs_c)
 
 def input_inject():
     print "We're good to go"
@@ -157,6 +250,13 @@ def input_inject_CIFAR():
 
 def test_inject_CIFAR():
     return inputsTst_fin_c, labelsTst_c
+
+def input_inject_PATH():
+    print "We're good to go"
+    return inputs_fin_p, labels_p
+
+def test_inject_PATH():
+    return inputsTst_fin_p, labelsTst_p
 
 # im = Image.new('RGB', (32,32))
 # pixel = im.load()
