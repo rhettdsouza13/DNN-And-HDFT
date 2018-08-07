1c88c288d1ae6314a6d01e4e5d9f3934431d6baa
Rhett D'souza <rhettdsouza13@gmail.com>
2018-08-07 19:06:41 -0400

    Saliency Maps Generated

--- trainer_tester.py
+++ trainer_tester.py
@@ -1,16 +1,30 @@
 import tensorflow as tf
 import numpy
 from functions import *
-# print "here"
 from parser import *
-# print "here"
 from PIL import Image
 from visualizer import *
-# print "here"
 from net_read_and_run import *
-# print "here"
 # from set_downsizer import *
-# print "here"
+import saliency
+from matplotlib import pylab as P
+
+
+def ShowGrayscaleImage(im, title='', ax=None):
+  if ax is None:
+    P.figure()
+  P.axis('off')
+
+  P.imshow(im, cmap=P.cm.gray, vmin=0, vmax=1)
+  P.title(title)
+
+def ShowImage(im, title='', ax=None):
+  if ax is None:
+    P.figure()
+  P.axis('off')
+  im = ((im + 1) * 127.5).astype(numpy.uint8)
+  P.imshow(im)
+  P.title(title)
 
 numpy.set_printoptions(threshold=numpy.nan)
 net_syn = sys.argv[1]
@@ -73,6 +87,8 @@ print Bias
 h_out_key = 'h' + str(len(fn_list))
 y = H[h_out_key]
 lR = tf.placeholder(tf.float32, shape=[])
+neuron_selector = tf.placeholder(tf.int32)
+y_neur = y[0][neuron_selector]
 
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
@@ -82,6 +98,7 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_
 train_step = tf.train.AdamOptimizer(learning_rate=lR).minimize(cross_entropy)
 #
 #GradientDescentOptimizer(0.001)
+prediction = tf.argmax(y,1)
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 casted = tf.cast(correct_prediction, tf.float32)
 accuracy = tf.reduce_mean(casted)
@@ -106,94 +123,123 @@ with tf.device('/cpu:0'):
     # inputsTst, labelsTst = test_inject_CIFAR()
 stopping = epochs
 bsize=100
-n_iters = (2500/bsize)
+n_iters = (3300/bsize)
 error_val = 100000
 step_cnt = 0
 val_data=[]
-with tf.Session() as sess:
+with tf.device('/device:GPU:0'):
+    with tf.Session() as sess:
 
-    # writer = tf.summary.FileWriter('output', graph=tf.get_default_graph())
+        # writer = tf.summary.FileWriter('output', graph=tf.get_default_graph())
 
-    sess.run(tf.global_variables_initializer())
-    # print inputs[0:1]
-    # print sess.run([reshaper, H['h0']], feed_dict={x:inputs[0:1]})
-    # print reshaper.get_shape()
-    # filname = '/home/hdft/Documents/DNN-DataNEW-Run-224-param-MIT/DATA_NETS_2018_' + str(net_num) + '.npy'
+        sess.run(tf.global_variables_initializer())
+        # print inputs[0:1]
+        # print sess.run([reshaper, H['h0']], feed_dict={x:inputs[0:1]})
+        # print reshaper.get_shape()
+        # filname = '/home/hdft/Documents/DNN-DataNEW-Run-224-param-MIT/DATA_NETS_2018_' + str(net_num) + '.npy'
 
-    # filname = '/home/hdft/Documents/DNN-DataNEW-Run-' + str(run_num) + '-' + str(dim_size) + '-MIT/DATA_NETS_2018_' + str(net_num) + '.npy'
+        # filname = '/home/hdft/Documents/DNN-DataNEW-Run-' + str(run_num) + '-' + str(dim_size) + '-MIT/DATA_NETS_2018_' + str(net_num) + '.npy'
 
-    data = numpy.array([[1,2,3,4]])
+        data = numpy.array([[1,2,3,4]])
 
-    print len(inputs)
+        print len(inputs)
 
-    for j in xrange(epochs):
-        print "Epoch: " + str(j)
-        val_accuracy, val_error = sess.run([accuracy, cross_entropy], feed_dict={x:inputs[bsize*25:bsize*33], y_:labels[bsize*25:bsize*33], lR:lR_val})
-        print('Validation accuracy %g Error %f \n' % (val_accuracy, val_error))
+        for j in xrange(epochs):
+            print "Epoch: " + str(j)
+            val_accuracy, val_error = sess.run([accuracy, cross_entropy], feed_dict={x:inputs[bsize*25:bsize*33], y_:labels[bsize*25:bsize*33], lR:lR_val})
+            print('Validation accuracy %g Error %f \n' % (val_accuracy, val_error))
 
-        val_data.append([val_accuracy, val_error, j, 1])
-        # if val_accuracy >= 0.98:
-        #     stopping = j
-        #     break
+            val_data.append([val_accuracy, val_error, j, 1])
+            # if val_accuracy >= 0.98:
+            #     stopping = j
+            #     break
 
-        if val_error > error_val:
-            step_cnt += 1
-        else:
-            step_cnt = 0
+            if val_error > error_val:
+                step_cnt += 1
+            else:
+                step_cnt = 0
 
-        if error_val > val_error:
-            error_val = val_error
+            if error_val > val_error:
+                error_val = val_error
 
-        # error_val = val_error
+            # error_val = val_error
 
-        print step_cnt
+            print step_cnt
 
-        if step_cnt == 5 or val_accuracy == 1:
-            break
-
-
-
-
-        for i in xrange(n_iters):
-            #
-            # test_accuracy, test_error = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[:2500], y_:labelsTst[:2500]})
-            #
-            # train_accuracy, error = sess.run([accuracy, cross_entropy], feed_dict={x:inputs[bsize*i:bsize*(i+1)], y_:labels[bsize*i:bsize*(i+1)]})
-            #
-            # data = numpy.concatenate((data, [[test_accuracy, test_error, train_accuracy, error]]), axis=0)
-            if i%1 == 0:
-
-                train_accuracy, error, learn = sess.run([accuracy, cross_entropy, lR], feed_dict={x:inputs[bsize*(i):bsize*(i+1)], y_:labels[bsize*(i):bsize*(i+1)], lR:lR_val})
-
-                data = numpy.concatenate((data, [[train_accuracy, error, (n_iters*j)+i, 0]]), axis=0)
-
-                # print('Step %d, Training accuracy %g Error %f Learning Rate %f \n' % (i, train_accuracy, error, learn))
-
-
-            train_step.run(feed_dict={x:inputs[bsize*(i):bsize*(i+1)], y_:labels[bsize*(i):bsize*(i+1)], lR:lR_val})
-        #lR_val *= 0.95
+            if step_cnt == 50 or val_accuracy == 1 or j>50:
+                break
 
 
 
-    # print "\nTest\n"
-    # ta_1, te_1 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[:2500], y_:labelsTst[:2500], lR:lR_val})
-    # ta_2, te_2 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[2500:5000], y_:labelsTst[2500:5000], lR:lR_val})
-    # ta_3, te_3 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[5000:7500], y_:labelsTst[5000:7500], lR:lR_val})
-    # ta_4, te_4 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[7500:], y_:labelsTst[7500:], lR:lR_val})
-    # test_accuracy = (ta_4+ta_3+ta_2+ta_1)*0.25
-    # test_error = (te_4+te_3+te_2+te_1)*0.25
-    # print "Accuracy = " + str(test_accuracy)
-    # print "Error = " + str(test_error)
-    # data = numpy.concatenate((data, [[test_accuracy, test_error, 0, 0]]), axis=0)
-    # data = numpy.concatenate((data, val_data), axis=0)
 
-    print "\nTest\n"
-    test_accuracy, test_error = sess.run([accuracy, cross_entropy], feed_dict={x:inputs[bsize*33:], y_:labels[bsize*33:], lR:lR_val})
-    print "Accuracy = " + str(test_accuracy)
-    print "Error = " + str(test_error)
-    data = numpy.concatenate((data, [[test_accuracy, test_error, 0, 0]]), axis=0)
-    data = numpy.concatenate((data, val_data), axis=0)
+            for i in xrange(n_iters):
+                #
+                # test_accuracy, test_error = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[:2500], y_:labelsTst[:2500]})
+                #
+                # train_accuracy, error = sess.run([accuracy, cross_entropy], feed_dict={x:inputs[bsize*i:bsize*(i+1)], y_:labels[bsize*i:bsize*(i+1)]})
+                #
+                # data = numpy.concatenate((data, [[test_accuracy, test_error, train_accuracy, error]]), axis=0)
+                if i%1 == 0:
 
-    # numpy.save(filname, data)
+                    train_accuracy, error, learn = sess.run([accuracy, cross_entropy, lR], feed_dict={x:inputs[bsize*(i):bsize*(i+1)], y_:labels[bsize*(i):bsize*(i+1)], lR:lR_val})
 
-    #writer.close()
+                    data = numpy.concatenate((data, [[train_accuracy, error, (n_iters*j)+i, 0]]), axis=0)
+
+                    # print('Step %d, Training accuracy %g Error %f Learning Rate %f \n' % (i, train_accuracy, error, learn))
+
+
+                train_step.run(feed_dict={x:inputs[bsize*(i):bsize*(i+1)], y_:labels[bsize*(i):bsize*(i+1)], lR:lR_val})
+            #lR_val *= 0.95
+
+
+
+        # print "\nTest\n"
+        # ta_1, te_1 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[:2500], y_:labelsTst[:2500], lR:lR_val})
+        # ta_2, te_2 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[2500:5000], y_:labelsTst[2500:5000], lR:lR_val})
+        # ta_3, te_3 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[5000:7500], y_:labelsTst[5000:7500], lR:lR_val})
+        # ta_4, te_4 = sess.run([accuracy, cross_entropy], feed_dict={x:inputsTst[7500:], y_:labelsTst[7500:], lR:lR_val})
+        # test_accuracy = (ta_4+ta_3+ta_2+ta_1)*0.25
+        # test_error = (te_4+te_3+te_2+te_1)*0.25
+        # print "Accuracy = " + str(test_accuracy)
+        # print "Error = " + str(test_error)
+        # data = numpy.concatenate((data, [[test_accuracy, test_error, 0, 0]]), axis=0)
+        # data = numpy.concatenate((data, val_data), axis=0)
+
+        print "\nTest\n"
+        test_accuracy, test_error = sess.run([accuracy, cross_entropy], feed_dict={x:inputs[bsize*33:], y_:labels[bsize*33:], lR:lR_val})
+        print "Accuracy = " + str(test_accuracy)
+        print "Error = " + str(test_error)
+        data = numpy.concatenate((data, [[test_accuracy, test_error, 0, 0]]), axis=0)
+        data = numpy.concatenate((data, val_data), axis=0)
+
+        # numpy.save(filname, data)
+
+        prediction_class = sess.run(prediction, feed_dict={x:inputs[1:2]})[0]
+        print prediction_class
+        #Construct the saliency object. This doesn't yet compute the saliency mask, it just sets up the necessary ops.
+        integrated_gradients = saliency.IntegratedGradients(tf.get_default_graph(), sess, y_neur, H['h0'])
+        im = sess.run(H['h0'], feed_dict={x:inputs[1:2]})[0]
+        # Baseline is a black image.
+        baseline = numpy.zeros(im.shape)
+        baseline.fill(-1)
+        # Compute the vanilla mask and the smoothed mask.
+        vanilla_integrated_gradients_mask_3d = integrated_gradients.GetMask(
+            im, feed_dict = {neuron_selector: prediction_class}, x_steps=25, x_baseline=baseline)
+        smoothgrad_integrated_gradients_mask_3d = integrated_gradients.GetSmoothedMask(
+            im, feed_dict = {neuron_selector: prediction_class}, x_steps=25, x_baseline=baseline)
+
+        # Call the visualization methods to convert the 3D tensors to 2D grayscale.
+        vanilla_mask_grayscale = saliency.VisualizeImageGrayscale(vanilla_integrated_gradients_mask_3d)
+        smoothgrad_mask_grayscale = saliency.VisualizeImageGrayscale(smoothgrad_integrated_gradients_mask_3d)
+
+        # Set up matplot lib figures.
+        ROWS = 1
+        COLS = 3
+        UPSCALE_FACTOR = 15
+        P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
+
+        # Render the saliency masks.
+        ShowImage(im,ax=P.subplot(ROWS, COLS, 1))
+        ShowGrayscaleImage(vanilla_mask_grayscale, title='Vanilla Integrated Gradients', ax=P.subplot(ROWS, COLS, 2))
+        ShowGrayscaleImage(smoothgrad_mask_grayscale, title='Smoothgrad Integrated Gradients', ax=P.subplot(ROWS, COLS, 3))
+        P.show()
